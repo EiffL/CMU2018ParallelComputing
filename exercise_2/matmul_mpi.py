518dd84680d2e700f5ff2245592b826fe81091bd
Francois <francois@orioncloud.fr>
2018-07-22 23:06:40 -0400

    Adds path to data stored on scratch

--- exercise_2/matmul_mpi.py
+++ exercise_2/matmul_mpi.py
@@ -7,14 +7,17 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+t0 = time.time()  # start time
+
 # Open the data file
-with h5py.File('data.hdf', 'r', driver='mpio', comm=MPI.COMM_WORLD) as f:
+with h5py.File('/global/cscratch1/sd/flanusse/data.hdf', 'r', driver='mpio',
+               comm=MPI.COMM_WORLD) as f:
     A_dset = f['A']
     B_dset = f['B']
 
     A = np.empty(A_dset.shape, A_dset.dtype)
     with A_dset.collective:
-       A[:] = A_dset[:]
+        A[:] = A_dset[:]
 
     nx, ny = B_dset.shape
     block_size = ny // size
@@ -22,16 +25,12 @@ with h5py.File('data.hdf', 'r', driver='mpio', comm=MPI.COMM_WORLD) as f:
     end = (rank + 1) * block_size
     B = np.empty((nx, block_size), B_dset.dtype)
     with B_dset.collective:
-       B[:] = B_dset[:, start:end]
+        B[:] = B_dset[:, start:end]
 
 C = np.empty((A.shape[0], ny), dtype=np.float32)
 buffer = np.empty((A.shape[0], B.shape[1]), dtype=np.float32)
 
-# Waiting for all processes to be ready
-comm.Barrier()
-t0 = time.time()  # start time
-
-#Compute the local dot product
+# Compute the local dot product
 C[:, start:end] = np.dot(A, B)
 
 buffer[:] = C[:, rank * block_size:(rank + 1) * block_size]
@@ -43,6 +42,6 @@ for i in range(size - 1):
     comm.Sendrecv_replace(buffer, dest=dest_rank, source=source_rank)
     C[:, ((source_rank + i) % size) * block_size:(((source_rank + i) % size) + 1) * block_size] = buffer
 
-t1 = time.time() # end time
+t1 = time.time()  # end time
 
-print("MPI matrix multiplication took %f ms" %((t1-t0)*1000.))
+print("MPI matrix multiplication took %f ms" % ((t1 - t0) * 1000.))
--- exercise_2/matmul_serial.py
+++ exercise_2/matmul_serial.py
@@ -3,12 +3,15 @@ import numpy as np
 import h5py
 import time
 
+t0 = time.time()  # start time
+
 # Open the data file
-with h5py.File('data.hdf', 'r') as f:
+with h5py.File('/global/cscratch1/sd/flanusse/data.hdf', 'r') as f:
     A = f['A'][:]
     B = f['B'][:]
 
-t0 = time.time()  # start time
-C = np.dot(A,B)
-t1 = time.time() # end time
-print("Numpy matrix multiplication took: %f ms"%((t1-t0)*1000))
+C = np.dot(A, B)
+
+t1 = time.time()  # end time
+
+print("IO and numpy matrix multiplication took: %f ms" % ((t1 - t0) * 1000))
